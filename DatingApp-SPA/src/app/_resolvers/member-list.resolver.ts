293f3fd27eac7bf963901f51e2c520935721d1fb
JLas2015 <jamallasri2012@gmail.com>
2018-12-27 15:38:09 +0100

    End of section 14

--- DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
@@ -8,13 +8,15 @@ import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class MemberListResolver implements Resolve<User[]> {
+    pageNumber = 1;
+    pageSize = 5;
 
     constructor(private userService: UserService, private router: Router, private alertify: AlertifyService) {}
 
     //#region Functions
     resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
         // Automatically subscribe
-        return this.userService.getUsers().pipe(
+        return this.userService.getUsers(this.pageNumber, this.pageSize).pipe(
             catchError(error => {
                 this.alertify.error('Problem retriebing data');
                 this.router.navigate(['/home']);
